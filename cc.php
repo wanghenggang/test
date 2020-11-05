2a8ab102f75f357bfa08a30fb40864fbda6d0f8b
wanghenggang <313582158@qq.com>
2020-11-05 17:11:12 +0800

    车

--- cc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once './vendor/autoload.php';
-
-use Trango\Test\Test;
-
-echo Test::cc();
--- src/Trango/Test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Trango\Test;
-
-
-class Test{
-    public static function cc(){
-        return 'this is a test file';
-    }
-}
